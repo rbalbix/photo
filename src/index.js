c4a845ec4e4e0d061a2e8a24f5c0dd3be738b91e
Ricardo Balbi <rbalbi@gmail.com>
2020-05-07 15:41:56 -0300

    feat(files): add method to remove junk files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const readlineSync = require('readline-sync');
 const fs = require('fs');
+const pathLib = require('path');
 const dir = require('node-dir');
 const isImage = require('is-image');
 const exif = require('exif-parser');
@@ -14,7 +15,7 @@ logger.level = 'debug';
 App to organize photos in an folder
 
 steps:
-. Give a nnamed folder
+. Give a named folder
 . Find the photo properties (date)
 . Separate the photos by folder on a MM/YYYY format
 
@@ -47,11 +48,20 @@ function parseFile(fullPath) {
 function createDir(parser, path, file) {
   let pathToCreate;
   if (parser == null) {
-    pathToCreate = `${path}/${moment(fs.statSync(file).mtime).format('YYYY')}/${moment(fs.statSync(file).mtime).format('MM')}`;
+    pathToCreate = `${path}/${moment(fs.statSync(file).mtime).format(
+      'YYYY'
+    )}/${moment(fs.statSync(file).mtime).format('MM')}`;
   } else {
-    pathToCreate = (parser.parse().tags.DateTimeOriginal !== undefined)
-      ? `${path}/${moment.unix(parser.parse().tags.DateTimeOriginal).format('YYYY')}/${moment.unix(parser.parse().tags.DateTimeOriginal).format('MM')}`
-      : `${path}/${moment(fs.statSync(file).mtime).format('YYYY')}/${moment(fs.statSync(file).mtime).format('MM')}`;
+    pathToCreate =
+      parser.parse().tags.DateTimeOriginal !== undefined
+        ? `${path}/${moment
+            .unix(parser.parse().tags.DateTimeOriginal)
+            .format('YYYY')}/${moment
+            .unix(parser.parse().tags.DateTimeOriginal)
+            .format('MM')}`
+        : `${path}/${moment(fs.statSync(file).mtime).format('YYYY')}/${moment(
+            fs.statSync(file).mtime
+          ).format('MM')}`;
   }
 
   if (!fs.existsSync(pathToCreate)) {
@@ -60,8 +70,30 @@ function createDir(parser, path, file) {
   return pathToCreate;
 }
 
-function organizePhotos(path, files) {
+// function deleteJunkFiles(path, files) {
+function deleteJunkFiles(path) {
+  try {
+    const files = dir.files(path, { sync: true });
+    files.forEach((file) => {
+      // logger.debug(file);
+      if (
+        ['.plist', '.heic', '.aae', '.thm', '.lrv'].includes(
+          pathLib.extname(file).toLowerCase()
+        )
+      ) {
+        fs.unlinkSync(file);
+        //file removed
+      }
+    });
+  } catch (err) {
+    logger.error(err);
+  }
+}
+
+// function organizePhotos(path, files) {
+function organizePhotos(path) {
   let pathToCreate;
+  const files = dir.files(path, { sync: true });
   files.forEach((file) => {
     if (fs.lstatSync(file).isFile()) {
       if (isImage(file)) {
@@ -82,14 +114,21 @@ function start() {
   // ask for folder
   const path = askFolderToReadPhotos();
   // read all photo files
-  dir.promiseFiles(path)
-    .then((files) => {
-      // read the properties of each file
-      organizePhotos(path, files);
-    })
-    .catch((e) => logger.error(e));
-  // read the properties of each file
-  // organizePhotos(path, files);
+  // dir
+  //   .promiseFiles(path)
+  //   .then((files) => {
+  //     // delete junk files
+  //     // [.plist, .heic, .aae, .thm, .lrv]
+  //     deleteJunkFiles(path, files);
+  //     // read the properties of each file
+  //     organizePhotos(path, files);
+  //   })
+  //   .catch((e) => logger.error(e));
+  // // read the properties of each file
+  // // organizePhotos(path, files);
+
+  deleteJunkFiles(path);
+  organizePhotos(path);
 }
 
 start();
