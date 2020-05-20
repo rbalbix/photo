3707e5b148fca54ce98c979e358b2c9d5224e256
Ricardo Balbi <rbalbi@gmail.com>
2020-05-20 10:33:36 -0300

    fix(package): fix config to create a pkg

--- test.js
+++
@@ -1,9 +0,0 @@
-const dir = require('node-dir');
-const fs = require('fs');
-
-dir.promiseFiles('C:/Users/rjunior/Desktop/Santa Fe - ABR_2019')
-  .then((files) => {
-    console.log(files);
-    files.map((file) => console.log(fs.statSync(file).mtime, fs.statSync(file).ctime, fs.statSync(file).atime, fs.statSync(file).birthtime));
-  })
-  .catch((e) => console.error(e));
